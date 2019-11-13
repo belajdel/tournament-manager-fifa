7a6009b80f2106572c9f01d128f5c7f3cec2d958
Eduardo Garza <eduardo@garza.ca>
2019-11-13 01:00:13 -0800

    v1 of home-nav

--- puntipin/src/components/HomeNavigation/HomeNavigation.tsx
+++ puntipin/src/components/HomeNavigation/HomeNavigation.tsx
@@ -1,33 +1,44 @@
 import React from 'react';
-import {
-  Link
-} from 'react-router-dom';
-import { NavigationContainer, ListItem } from './HomeNavigationStyles';
+import { 
+  NavigationContainer, 
+  NavHeader,
+  NavList,
+  ListItemLink,
+  ListItem, 
+  AuthList,
+  NavigatorContainer
+} from './HomeNavigationStyles';
 
 const HomeNavigation: React.FC = () => {
-
-  const ListStyle = {
-    display: 'flex'
-  };
-  
   return (
     <NavigationContainer>
-      <ul style={ListStyle}>
 
-        <ListItem>
-          <Link to="/">Home</Link>
-        </ListItem>
-        <ListItem>
-          <Link to="/about">About</Link>
-        </ListItem>
-        <ListItem>
-          <Link to="/login">Log in</Link>
-        </ListItem>
-        <ListItem>
-          <Link to="/signup">Sign up</Link>
-        </ListItem>
+      <NavHeader>
+        puntipin
+      </NavHeader>
       
-      </ul>
+      {/* Navigator Container */}
+      <NavigatorContainer>
+        <NavList>
+          <ListItem>
+            <ListItemLink to="/">Home</ListItemLink>
+          </ListItem>
+          <ListItem>
+            <ListItemLink to="/about">About</ListItemLink>
+          </ListItem>
+        </NavList>
+
+        <AuthList>
+          <ListItem>
+            <ListItemLink to="/login">Log in</ListItemLink>
+          </ListItem>
+          <ListItem>
+            <ListItemLink to="/signup">Sign up</ListItemLink>
+          </ListItem>
+        </AuthList>
+      </NavigatorContainer>
+      {/* End of Navigator Container */}
+      
     </NavigationContainer>
   );
 }
