aa25311eb8a03a9eb6ece395d0dd3672bd204374
Eduardo <eduardo@garza.ca>
2019-11-30 09:55:48 -0800

    working on settings

--- client/src/components/AccountModule/Settings/Settings.tsx
+++ client/src/components/AccountModule/Settings/Settings.tsx
@@ -1,11 +1,60 @@
 import React from 'react';
+import { Container } from '../../../styles/Global';
+import {
+  BoxHeader,
+  SettingsWrapper,
+  SettingsContainer,
+  SettingsLeft,
+  SettingsRight,
+  SettingsBox
+} from './SettingsStyles';
 
-const Settings: React.FC = () => {
-  return (
-    <div className="Settings">
-      <h1>Settings</h1>
-    </div>
-  );
+class Settings extends React.Component {
+  render() {
+    return (
+      <SettingsWrapper>
+        <Container>
+          <SettingsContainer>
+
+            <SettingsLeft>
+
+              <SettingsBox>
+                
+              </SettingsBox>
+
+              <SettingsBox>
+                <BoxHeader>
+                  Change Email
+                </BoxHeader>
+
+              </SettingsBox>
+
+              <SettingsBox>
+                <BoxHeader>
+                  Change Password
+                </BoxHeader>
+              </SettingsBox>
+
+            </SettingsLeft>
+            <SettingsRight>
+
+              <SettingsBox>
+                
+              </SettingsBox>
+
+              <SettingsBox>
+                <BoxHeader>
+                  Link Accounts
+                </BoxHeader>
+              </SettingsBox> 
+              
+            </SettingsRight>
+            
+          </SettingsContainer>
+        </Container>
+      </SettingsWrapper>
+    );
+  }
 }
 
 export default Settings;
---
+++ client/src/components/AccountModule/Settings/SettingsStyles.ts
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+export const SettingsWrapper = styled.section`
+  background: #52AA5E;
+`;
+
+export const SettingsContainer = styled.section`
+  /* background: #044B7F; */
+  background: #F4F4F4;
+  height: 100vh;
+`;
+
+export const SettingsLeft = styled.div`
+  /* ... */
+`;
+
+export const SettingsRight = styled.div`
+  /* ... */
+`;
+
+export const SettingsBox = styled.div`
+  border-bottom: 1px solid #CCC;
+  padding: 20px;
+`;
+
+export const BoxHeader = styled.h1`
+  color: #333;
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 18px;
+  text-align: center;
+`;
