69b82255072bb02dd356c976b8706bcae1f9e3b7
Eduardo <eduardo@garza.ca>
2019-12-02 17:36:17 -0800

    settings v1

--- client/src/components/AccountModule/Settings/Settings.tsx
+++ client/src/components/AccountModule/Settings/Settings.tsx
@@ -2,12 +2,23 @@ import React from 'react';
 import { Container } from '../../../styles/Global';
 import {
   BoxHeader,
+  Input,
+  InputContainer,
+  ThirdPartyIconContainer,
   SettingsWrapper,
   SettingsContainer,
   SettingsLeft,
   SettingsRight,
-  SettingsBox
+  SettingsBox,
+  ThirdPartyContainer,
+  ThirdPartyFacebook,
+  ThirdPartyGoogle,
+  ThirdPartyBox,
+  ThirdPartyIcon,
+  ThirdPartyButton
 } from './SettingsStyles';
+
+const ElonMuskImg = 'https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTY2MzU3Nzk2OTM2MjMwNTkx/elon_musk_royal_society.jpg';
 
 class Settings extends React.Component {
   render() {
@@ -19,7 +30,16 @@ class Settings extends React.Component {
             <SettingsLeft>
 
               <SettingsBox>
-                
+                <BoxHeader>
+                  Account basics
+                </BoxHeader>
+                <InputContainer>
+                  <Input 
+                    name="accountName"
+                    placeholder="Account name"
+                    type="text"
+                  />
+                </InputContainer>
               </SettingsBox>
 
               <SettingsBox>
@@ -27,18 +47,76 @@ class Settings extends React.Component {
                   Change Email
                 </BoxHeader>
 
+                <InputContainer>
+                  <Input 
+                    name="currentEmail"
+                    placeholder="Current email address"
+                    type="email"
+                  />
+                </InputContainer>
+
+                <InputContainer>
+                  <Input 
+                    name="secondaryEmail"
+                    placeholder="Secondary email address"
+                    type="email"
+                  />
+                </InputContainer>
+
               </SettingsBox>
 
               <SettingsBox>
                 <BoxHeader>
                   Change Password
                 </BoxHeader>
+                
+                <InputContainer>
+                  <Input 
+                    name="currentPassword"
+                    placeholder="Current password"
+                    type="password"
+                  />
+                </InputContainer>
+
+                <InputContainer>
+                  <Input 
+                    name="newPassword"
+                    placeholder="Create a new password"
+                    type="password"
+                  />
+                </InputContainer>
+
+                <InputContainer>
+                  <Input 
+                    name="newPasswordConfirm"
+                    placeholder="Confirm your new password"
+                    type="password"
+                  />
+                </InputContainer>
+                
               </SettingsBox>
 
             </SettingsLeft>
             <SettingsRight>
 
               <SettingsBox>
+
+                <InputContainer>
+                  <Input 
+                    name="favoriteTeam"
+                    placeholder="Favourite team"
+                    type="text"
+                  />
+                </InputContainer>
+
+                <InputContainer>
+                  <select>
+                    <option value="bundesliga">Bundesliga</option>
+                    <option value="mls">MLS</option>
+                    <option value="premierLeague">Premier League</option>
+                    <option value="serieA">Serie A</option>
+                  </select>
+                </InputContainer>
                 
               </SettingsBox>
 
@@ -46,7 +124,37 @@ class Settings extends React.Component {
                 <BoxHeader>
                   Link Accounts
                 </BoxHeader>
-              </SettingsBox> 
+                <ThirdPartyBox>
+
+                  <ThirdPartyContainer>
+                    <header>
+                      <ThirdPartyIconContainer>
+                        <ThirdPartyIcon 
+                          src={ElonMuskImg}
+                        />
+                      </ThirdPartyIconContainer>
+                      <h1>Eduardo Garza</h1>
+                    </header>
+                    <ThirdPartyFacebook>
+                      Unlink Facebook
+                    </ThirdPartyFacebook>
+                  </ThirdPartyContainer>
+
+                  <ThirdPartyContainer>
+                    <header>
+                      <ThirdPartyIconContainer>
+
+                      </ThirdPartyIconContainer>
+                      <h1>Account name</h1>
+                    </header>
+                    <ThirdPartyGoogle>
+                      Link Google
+                    </ThirdPartyGoogle>
+                  </ThirdPartyContainer>
+                  
+                </ThirdPartyBox> 
+              </SettingsBox>
+
               
             </SettingsRight>
             
--- client/src/components/AccountModule/Settings/SettingsStyles.ts
+++ client/src/components/AccountModule/Settings/SettingsStyles.ts
@@ -7,20 +7,22 @@ export const SettingsWrapper = styled.section`
 export const SettingsContainer = styled.section`
   /* background: #044B7F; */
   background: #F4F4F4;
+  display: flex;
   height: 100vh;
+  padding: 40px 20px 30px 20px;
 `;
 
 export const SettingsLeft = styled.div`
-  /* ... */
+  flex: 1;
 `;
 
 export const SettingsRight = styled.div`
-  /* ... */
+  flex: 1;
 `;
 
 export const SettingsBox = styled.div`
-  border-bottom: 1px solid #CCC;
   padding: 20px;
+  margin-bottom: 20px;
 `;
 
 export const BoxHeader = styled.h1`
@@ -28,4 +30,78 @@ export const BoxHeader = styled.h1`
   font-family: Arial, Helvetica, sans-serif;
   font-size: 18px;
   text-align: center;
+`;
+
+/**
+ * Input
+ */
+
+export const InputContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 20px 0;
+`;
+
+export const Input = styled.input`
+  border: 1px solid #CCC;
+  border-radius: 5px;
+  padding: 8px;
+  width: 80%;
+
+  &:focus {
+    outline: none;
+  }
+`;
+
+/**
+ * 
+ */
+export const ThirdPartyBox = styled.div`
+  display: flex;
+  background: lightgray;
+  justify-content: space-around;
+  margin: 20px 0;
+`;
+
+export const ThirdPartyContainer = styled.div`
+
+  & > header {
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 18px;
+    text-align: center;
+    margin: 10px 0;
+  }
+`;
+
+export const ThirdPartyIconContainer= styled.div`
+  border: 1px solid red;
+  border-radius: 50%;
+  height: 150px;
+  overflow: hidden;
+  width: 150px;
+`;
+
+export const ThirdPartyIcon = styled.img`
+  display: block;
+  object-fit: contain;
+  height: 100%;
+  /* width: 100%; */
+`;
+
+export const ThirdPartyButton = styled.button`
+  border: none;
+  background: none;
+  margin: 20px;
+  color: white;
+  padding: 12px 20px;
+  border-radius: 5px;
+  cursor: pointer;
+`;
+
+export const ThirdPartyFacebook = styled(ThirdPartyButton)`
+  background: #1778F2;
+`;
+
+export const ThirdPartyGoogle = styled(ThirdPartyButton)`
+  background: #DB4437;
 `;
